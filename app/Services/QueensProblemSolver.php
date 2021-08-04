d929908c996f650f9ec151bda0734c7ef4ab07c6
ArnoCB <a.bouwes@upcmail.nl>
2021-08-04 10:30:35 +0200

    upd make queens problem solver code more modern

--- app/Services/QueensProblemSolver.php
+++ app/Services/QueensProblemSolver.php
@@ -1,15 +1,30 @@
 <?php
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
+
 class QueensProblemSolver
 {
     /**
+     * Store in cache to avoid unnecessary recalculations.
+     *
+     * @return array    The solutions in pairs of FEN 8x8 and FEN 7x7 solutions
+     */
+    public function solveQueensProblem(): array
+    {
+        return Cache::rememberForever('solutionsInFen', function () {
+
+            return $this->generateSolutions();
+        });
+    }
+
+    /**
      * Return all solutions for the 7x7 Queens problem in an
-     * array containing both the 8x8 and 7x7 Fen codes for all solutions
+     * array containing both the 8x8 and 7x7 Fen codes for all solutions.
      *
      * @return array
      */
-    public function solveQueensProblem(): array
+    private function generateSolutions(): array
     {
         // There can be only one queen per rank or file.
         // The position in the array can be seen as the
@@ -22,9 +37,9 @@ class QueensProblemSolver
         // Array to keep account of all solutions stil in contention,
         // and the constraints for the following ranks.
         // For the first rank, all files are still possible.
-        $solution_paths = array_map(static function ($x) {
-            return [[$x], self::addConstraints(1, $x)];
-        }, $board_ranks);
+        $solution_paths = array_map(
+            static fn($x) => [[$x], $this->addConstraints(1, $x, [], $board_size)], $board_ranks
+        );
 
         for ($i = 2; $i <= $board_size; $i++) {
 
@@ -47,57 +62,17 @@ class QueensProblemSolver
                     $new_path[] = $file;
 
                     // If this is not the final solution, keep track of
-                    // the constraints.
-                    if ($i < $board_size) {
-
-                        $solution_paths_extended[] = [
-                            $new_path,
-                            self::addConstraints($rank_nr, $file, $constraints)
-                        ];
-                    }
-                    else {
-
-                        // If this is the final solution, only the solution is important
-                        $solution_paths_extended[] = $new_path;
-                    }
+                    // the constraints. If not, only keep the solutions.
+                    $solution_paths_extended[] = ($i < $board_size)
+                        ? [$new_path, $this->addConstraints($rank_nr, $file, $constraints, $board_size)]
+                        : $new_path;
                 }
             }
 
             $solution_paths = $solution_paths_extended;
         }
 
-        $fen_solutions = [];
-
-        foreach ($solution_paths as $solution) {
-
-            // Start with an empty row, since we have display a 7x7 solution on a 8x8 board
-            $fen_board8 = '8/';
-
-            // Also produce the fen code for a 7x7 board. This makes the symmetries easier
-            // to spot.
-            $fen_board7 = '';
-
-            foreach ($solution as $iValue) {
-
-                // generate the 8x8 fen code for this solution, so
-                // the solutions can be displayed on a chessboard.
-                $iValue--;
-                $fen_start = $iValue !== 0 ? $iValue : '';
-                $fen_end = (6-$iValue) !== 0 ? (6-$iValue) : '';
-
-                $fen_row = $fen_start . 'Q' . (7-$iValue) . '/';
-                $fen_row7 = $fen_start . 'Q' . $fen_end . '/';
-
-                $fen_board7 .= $fen_row7;
-                $fen_board8 .= $fen_row;
-            }
-
-            // remove the unnecessary slash at the end of the string
-            $fen_solutions[] = [substr($fen_board8,0, -1),
-                                substr($fen_board7,0, -1)];
-        }
-
-        return $fen_solutions;
+        return $this->createFenSolutions($solution_paths);
     }
 
     /**
@@ -112,7 +87,7 @@ class QueensProblemSolver
      * @param int $board_size
      * @return array                an array with all constraints
      */
-    private static function addConstraints(int $rank, int $file, array $constraints = [], int $board_size = 7): array
+    private function addConstraints(int $rank, int $file, array $constraints = [], int $board_size = 7): array
     {
         for ($i = $rank + 1; $i <= $board_size; $i++) {
 
@@ -142,5 +117,47 @@ class QueensProblemSolver
         }
 
         return $constraints;
+    }
+
+    /**
+     * Convert the solutions into Forsyth-Edwards Notation (FEN)
+     *
+     * @param $solution_paths
+     * @return array
+     */
+    private function createFenSolutions($solution_paths): array
+    {
+        $fen_solutions = [];
+
+        foreach ($solution_paths as $solution) {
+
+            // Start with an empty row, since we have to display a 7x7 solution on a 8x8 board
+            $fen_board8 = '8/';
+
+            // Also produce the fen code for a 7x7 board. This makes the symmetries easier
+            // to spot.
+            $fen_board7 = '';
+
+            foreach ($solution as $iValue) {
+
+                // generate the 8x8 fen code for this solution, so
+                // the solutions can be displayed on a chessboard.
+                $iValue--;
+                $fen_start = $iValue !== 0 ? $iValue : '';
+                $fen_end = (6-$iValue) !== 0 ? (6-$iValue) : '';
+
+                $fen_row7 = $fen_start . 'Q' . $fen_end . '/';
+                $fen_row8 = $fen_start . 'Q' . (7-$iValue) . '/';
+
+                $fen_board7 .= $fen_row7;
+                $fen_board8 .= $fen_row8;
+            }
+
+            // remove the unnecessary slash at the end of the string
+            $fen_solutions[] = [substr($fen_board8,0, -1),
+                                substr($fen_board7,0, -1)];
+        }
+
+        return $fen_solutions;
     }
 }
